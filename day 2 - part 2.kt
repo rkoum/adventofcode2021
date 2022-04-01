3db80e0652aade7b6ac7f79a834a88db514ad7ea
rkoum <95508360+rkoum@users.noreply.github.com>
2022-04-01 22:30:18 +0300

    Create day 2 - part 2.kt

---
+++ day 2 - part 2.kt
@@ -0,0 +1,24 @@
+import java . io . File
+import java . io . InputStream
+
+fun main(args: Array<String>) {
+    val inputStream: InputStream = File("C:\\Users\\aktex\\Documents\\forw.txt").inputStream()
+    val lineList = mutableListOf<String>()
+    inputStream.bufferedReader().forEachLine { lineList.add(it.toString()) }
+    var horizontal = 0
+    var depth = 0
+    var aim = 0
+    for (n in 0..lineList.size - 1) {
+        val str = lineList[n]
+        var num = str.split(" ")[1].toInt()
+        var direction = str.split(" ")[0]
+        when (direction) {
+            "forward" -> {horizontal = horizontal + num
+                depth += num * aim 
+            }
+            "up" -> aim -= num
+            "down" -> aim += num
+        }
+    }
+    println(horizontal*depth)
+}
